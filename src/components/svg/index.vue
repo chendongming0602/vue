eeabc4a5c00c83b249313b634be8b9ca80d62a77
陈栋明 <chendm0602@163.com>
2020-12-18 17:54:17 +0800

    添加svg阿里图标

---
+++ src/components/svg/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg :class="svgClass" aria-hidden="true" v-on="$listeners">
+    <use :xlink:href="iconName"  />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'SvgIcon',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    iconName() {
+      return `#icon-${this.name}`
+    },
+    svgClass() {
+      if (this.className) {
+        return 'svg-icon ' + this.className
+      } else {
+        return 'svg-icon'
+      }
+    },
+  }
+}
+</script>
+<style >
+  .svg-icon,.ss{
+    width: 2rem;
+    height: 2rem;
+    color: #44884a !important;
+    fill: currentColor;
+  }
+</style>
