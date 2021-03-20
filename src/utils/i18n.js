183787990228070fa14a20f6b458252e94178704
chendongming <chendm0602@163.com>
2021-03-20 23:24:02 +0800

    添加 多语言切换

---
+++ src/utils/i18n.js
@@ -0,0 +1,17 @@
+export const messages={
+    "zh-CN":{
+        name:"中文语音包"
+    },
+    "en-US":{
+        name:"英文语音包"
+    }
+}
+/*  //修改语言
+    this.$i18n.locale="zh-CN"
+    //html使用语言
+    $t("name")
+    //js使用语言
+    this.$i18n.tc("name")
+    //时间/各种格式请查看i18n文档
+    https://kazupon.github.io/vue-i18n/zh/started.html#html
+*/ 
